9b932d7e09a1883043177e1d8bf5c8ada14df432
Scott Chamberlain <myrmecocystus@gmail.com>
2016-04-01 11:01:21 -0700

    fixes, import lawn

--- R/geojson-package.R
+++ R/geojson-package.R
@@ -2,6 +2,7 @@
 #'
 #' @importFrom jsonlite fromJSON toJSON
 #' @importFrom jqr jq
+#' @importFrom lawn lawn_within
 #' @name geojson-package
 #' @aliases geojson
 #' @docType package
--- R/overlay.R
+++ R/overlay.R
@@ -1,8 +1,6 @@
-#' Search geojson
+#' Overlay
 #' @export
 #' @examples
-#' x <- "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[-99.74,32.45]},\"properties\":{}}]}"
-#' x <- as.geojson(x)
 #' library("lawn")
 #' lawn_data$points_within %>% view
 #' overlay(lawn_data$points_within, lawn_data$polygons_within) %>% view
