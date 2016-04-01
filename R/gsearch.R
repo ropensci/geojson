75fae65acda588c62ed9a1cff56de4a93d8299cd
Scott Chamberlain <myrmecocystus@gmail.com>
2016-04-01 11:32:08 -0700

    more exmamples for search

--- R/gsearch.R
+++ R/gsearch.R
@@ -4,6 +4,19 @@
 #' x <- "{\"type\":\"FeatureCollection\",\"features\":[{\"type\":\"Feature\",\"geometry\":{\"type\":\"Point\",\"coordinates\":[-99.74,32.45]},\"properties\":{}}]}"
 #' x <- as.geojson(x)
 #' gsearch(x, ".")
+#' gsearch(x, "keys")
+#'
+#' library("lawn")
+#' x <- lawn_data$polygons_average
+#' x <- as.geojson(x)
+#' gsearch(x, ".")
+#' gsearch(x, "keys")
+#' gsearch(x, ".[]")
+#' gsearch(x, ".features")
+#' gsearch(x, ".features[]")
+#' gsearch(x, ".features[].type")
+#' gsearch(x, ".features[].properties")
+#' gsearch(x, ".features[].geometry[]")
 gsearch <- function(x, y) {
   jqr::jq(unclass(x), y)
 }
