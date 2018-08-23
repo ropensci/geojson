0c42a00d93ba1400992a203d527657a6025e370b
Carson Sievert <cpsievert1@gmail.com>
2018-08-23 09:39:48 -0500

    conditionally set sf method on package load

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,3 +1,9 @@
 .onLoad <- function(libname, pkgname) {
   linting_opts()
+
+  if (requireNamespace('sf', quietly = TRUE)) {
+    setMethod("as.geojson", "sf", function(x) {
+      as.geojson(sf::as_Spatial(x))
+    })
+  }
 }
