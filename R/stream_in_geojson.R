cbb98b91fbdc298e67dc42bb06ab75c1eadf6d68
Scott Chamberlain <myrmecocystus@gmail.com>
2019-01-11 17:46:32 -0800

    more work on ndgeojson fxns, tests for stream_in_geojson internal fxn and for ndgeo fxns #36 #31

--- R/stream_in_geojson.R
+++ R/stream_in_geojson.R
@@ -2,18 +2,21 @@
 #' @noRd 
 #' @param con a connection. closed on exit. make a new connection if you reuse
 #' the same file on subsequent uses. required
-#' @param pagesize number of lines to process in each loop. default: 500
+#' @param pagesize (integer) number of lines to process in each loop. 
+#' default: 500
+#' @param verbose (logical) print messages. default: `TRUE`
 #' @examples \dontrun{
 #' file <- system.file("examples", 'ndgeojson1.json', package = "geojson")
 #' con <- file(file)
 #' con
 #' stream_in_geojson(con)
 #' }
-stream_in_geojson <- function(con, pagesize = 500) {
+stream_in_geojson <- function(con, pagesize = 500, verbose = TRUE) {
   if (!inherits(con, "connection")) {
     stop("Argument 'con' must be a connection.")
   }
   stopifnot(inherits(pagesize, c('integer', 'numeric')))
+  stopifnot(inherits(verbose, 'logical'))
 
   res <- character()
   count <- 0
@@ -23,12 +26,12 @@ stream_in_geojson <- function(con, pagesize = 500) {
   }
 
   if (!isOpen(con, "r")) {
-    message("opening ", class(con)[1L]," input connection.")
+    if (verbose) message("opening ", class(con)[1L]," input connection.")
 
     # binary connection prevents recoding of utf8 to latin1 on windows
     open(con, "rb")
     on.exit({
-      message("closing ", class(con)[1L]," input connection.")
+      if (verbose) message("closing ", class(con)[1L]," input connection.")
       close(con)
     })
   }
@@ -42,8 +45,9 @@ stream_in_geojson <- function(con, pagesize = 500) {
       # clean up text sequences special character
       cleanpage <- gsub("\036", "", cleanpage)
       # process
-      counter(Map(to_feature, cleanpage, extract_features(cleanpage)))
-      cat("\r Found", count, "records...")
+      counter(unlist(unname(
+        Map(to_feature, cleanpage, extract_features(cleanpage)))))
+      if (verbose) cat("\r Found", count, "records...")
     }
     if (length(page) < pagesize) break
   }
---
+++ tests/testthat/test-ndgeo.R
@@ -0,0 +1,53 @@
+context("ndgeo_write")
+
+file <- system.file("examples", 'featurecollection2.geojson',
+  package = "geojson")
+str <- paste0(readLines(file), collapse = " ")
+x <- featurecollection(str)
+
+test_that("ndgeo_write", {
+  outfile <- tempfile(fileext = ".geojson")
+  aa <- ndgeo_write(x, outfile)
+  fconn <- file(outfile)
+  bb <- readLines(outfile)
+
+  expect_null(aa)
+  expect_true(grepl("FeatureCollection", str))
+  expect_false(any(grepl("FeatureCollection", bb)))
+  expect_equal(length(str), 1)
+  expect_equal(length(bb), 3)
+
+  close(fconn)
+})
+
+test_that("ndgeo_write fails well", {
+  expect_error(ndgeo_write(), "argument \"x\" is missing")
+  expect_error(ndgeo_write(5), "no 'ndgeo_write' method for numeric")
+  expect_error(ndgeo_write(x, 5), "is not TRUE")
+  expect_error(ndgeo_write(x, tempfile(), 5), "invalid 'sep' argument")
+})
+
+
+
+context("ndgeo_read")
+
+test_that("ndgeo_read", {
+  file <- system.file("examples", 'ndgeojson1.json', package = "geojson")
+  aa <- ndgeo_read(file, verbose = FALSE)
+
+  expect_is(aa, "geojson")
+  expect_is(unclass(aa), "character")
+  expect_true(nzchar(aa))
+  expect_match(aa, "FeatureCollection")
+  expect_match(aa, "features")
+  expect_match(aa, "coordinates")
+  expect_equal(length(aa), 1)
+})
+
+test_that("ndgeo_read fails well", {
+  expect_error(ndgeo_read(), "argument \"txt\" is missing")
+  expect_error(ndgeo_read(5), "'txt' must be a string, URL or file.")
+  expect_error(ndgeo_read('x', 'x'), "Argument 'con' must be a connection")
+  expect_error(ndgeo_read(file(tempfile()), 'x'), "is not TRUE")
+})
+
