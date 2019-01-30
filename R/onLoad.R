890998d257d094c17fa04a66d44a0bdd468c8564
Scott Chamberlain <myrmecocystus@gmail.com>
2019-01-29 19:00:02 -0800

    nocov comments for onload

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,9 +1,9 @@
-.onLoad <- function(libname, pkgname) {
+.onLoad <- function(libname, pkgname) { # nocov start
   linting_opts()
 
-  if (requireNamespace('sf', quietly = TRUE)) {
+  if (requireNamespace("sf", quietly = TRUE)) {
     setMethod("as.geojson", "sf", function(x) {
       as.geojson(sf::as_Spatial(x))
     })
   }
-}
+} # nocov end
